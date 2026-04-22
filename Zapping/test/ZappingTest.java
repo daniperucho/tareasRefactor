098aa42aa076a41dfb3806a5c220998559b58a9f
daniperucho <adperucho@gmail.com>
2026-04-22 21:15:31 +0200

    test 10 canal del 2 al 99 y refactorización

--- Zapping/test/ZappingTest.java
+++ Zapping/test/ZappingTest.java
@@ -12,4 +12,14 @@ class ZappingTest {
     void testCanal1al2() {
         assertEquals(1, Zapping.clics(1, 2));
     }
+
+    @Test
+    void testCanal10al30() {
+        assertEquals(20, Zapping.clics(10, 30));
+    }
+
+    @Test
+    void testCanal2al99() {
+        assertEquals(2, Zapping.clics(99, 2));
+    }
 }
