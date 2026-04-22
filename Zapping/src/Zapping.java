ac487795dde75cc3b7ccec4fd53ba0d899477370
daniperucho <adperucho@gmail.com>
2026-04-22 19:41:10 +0200

    commit inicial primer test canal 0 al 0

---
+++ Zapping/src/Zapping.java
@@ -0,0 +1,6 @@
+public class Zapping {
+
+    public static int clics(int subirCanales, int bajarCanales) {
+        return 0;
+    }
+}
---
+++ Zapping/test/ZappingTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ZappingTest {
+
+    @Test
+    void testCanal0al0() {
+        assertEquals(0, Zapping.clics(0, 0));
+    }
+}
+
