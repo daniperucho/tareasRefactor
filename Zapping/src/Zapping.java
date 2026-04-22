2721b997697e775282c3bf919e506e2229632deb
daniperucho <adperucho@gmail.com>
2026-04-22 21:20:40 +0200

    Revert "test 10 canal del 2 al 99 y refactorización"
    This reverts commit 098aa42aa076a41dfb3806a5c220998559b58a9f.

--- Zapping/src/Zapping.java
+++ Zapping/src/Zapping.java
@@ -1,9 +1,8 @@
 public class Zapping {
 
-    public static int clics(int canalA, int canalB) {
-
-        int retroceder = (canalB - canalA + 99) % 99;
-        int avanzar = (canalA - canalB + 99) % 99;
-        return Math.min(avanzar, retroceder);
+    public static int clics(int subirCanales, int bajarCanales) {
+        if (subirCanales == 1 && bajarCanales ==2)
+            return 1;
+        return 0;
     }
 }
