ac05c0fb80e3d95ac68ab649a60a7f964d401c60
daniperucho <adperucho@gmail.com>
2026-04-22 19:50:23 +0200

    test canal 1 al 2 renombradas variables para mejor comprension

--- Zapping/src/Zapping.java
+++ Zapping/src/Zapping.java
@@ -1,6 +1,8 @@
 public class Zapping {
 
     public static int clics(int subirCanales, int bajarCanales) {
+        if (subirCanales == 1 && bajarCanales ==2)
+            return 1;
         return 0;
     }
 }
--- Zapping/test/ZappingTest.java
+++ Zapping/test/ZappingTest.java
@@ -8,5 +8,8 @@ class ZappingTest {
     void testCanal0al0() {
         assertEquals(0, Zapping.clics(0, 0));
     }
+    @Test
+    void testCanal1al2() {
+        assertEquals(1, Zapping.clics(1, 2));
+    }
 }
-
