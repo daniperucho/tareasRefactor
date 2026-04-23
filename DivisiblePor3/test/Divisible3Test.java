7b5b3b6454e340bd0d994ad13f363f0b9675450b
daniperucho <adperucho@gmail.com>
2026-04-23 15:26:24 +0200

    test 2

--- DivisiblePor3/test/Divisible3Test.java
+++ DivisiblePor3/test/Divisible3Test.java
@@ -6,7 +6,12 @@ class Divisible3Test {
 
     @Test
     void test1() {
-    assertEquals("Sí", Divisible3.esDivisible(2));
+        assertEquals("Sí", Divisible3.esDivisible(2));
+    }
+
+    @Test
+    void test2() {
+        assertEquals("Sí", Divisible3.esDivisible(6));
     }
 
 }
