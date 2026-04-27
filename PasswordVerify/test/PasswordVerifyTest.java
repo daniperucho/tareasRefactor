62f53ff7fcf6be77a08665da9de05e70e80b0084
daniperucho <adperucho@gmail.com>
2026-04-27 23:05:49 +0200

    test 2 password hola

--- PasswordVerify/test/PasswordVerifyTest.java
+++ PasswordVerify/test/PasswordVerifyTest.java
@@ -1,4 +1,4 @@
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,6 +10,15 @@ public class PasswordVerifyTest {
         assertFalse(r.valido);
         assertEquals("La contrasenya ha de tenir almenys 8 caràcters\n" + "La contrasenya ha de contenir almenys 2 números\n" +
                 "La contrasenya ha de contenir almenys una lletra majúscula\n" + "La contrasenya ha de contenir almenys un caràcter especial",
+                r.errores);
+    }
+
+    @Test
+    public void testHola() {
+        PasswordVerify.PasswordResultado r = PasswordVerify.valido("hola");
+        assertFalse(r.valido);
+        assertEquals("La contrasenya ha de tenir almenys 8 caràcters\n" + "La contrasenya ha de contenir almenys 2 números\n" +
+                        "La contrasenya ha de contenir almenys una lletra majúscula\n" + "La contrasenya ha de contenir almenys un caràcter especial",
                 r.errores);
     }
 }
